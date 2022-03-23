9b1927293d829afd804f0ea85a5de0fb89a71df2
Myriam Mornet <dahisland@gmail.com>
2022-03-23 02:13:49 +0100

    Suppr body.style.overflow in loader function

--- scripts/utils/loader.js
+++ scripts/utils/loader.js
@@ -4,9 +4,7 @@ const loader = document.querySelector(".loader");
 document.onreadystatechange = function () {
   if (document.readyState === "complete") {
     loader.style.display = "none";
-    body.style.overflow = "auto";
   } else {
     loader.style.display = "flex";
-    body.style.overflow = "hidden";
   }
 };
